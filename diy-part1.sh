6559ab635de250f8accc57e62ae925158f5a2a2c
Ljzkirito <kirito.ljz@gmail.com>
2021-02-02 21:11:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,21 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-# 清除旧版argon主题并拉取最新版
-rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # Remove UnblockNeteaseMusicGo upx commands
 sed -i "/upx/d" package/lean/UnblockNeteaseMusicGo/Makefile || true
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/lean/OpenClash
+# Add luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/lean/luci-app-adguardhome
+# Add luci-app-diskman
+git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman package/lean/luci-app-diskman
+mkdir package/lean/parted
+cp package/lean/luci-app-diskman/Parted.Makefile package/lean/parted/Makefile
+# Add luci-app-dockerman
+rm -rf package/lean/luci-app-docker
+git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman  package/lean/luci-app-dockerman
+git clone --depth=1 https://github.com/lisaac/luci-lib-docker package/lean/luci-lib-docker
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
