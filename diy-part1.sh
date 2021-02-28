6a27df839498bb470d5d2097d00bc1bf53727212
Ljzkirito <kirito.ljz@gmail.com>
2021-02-28 22:09:51 +0800

    give up Local version

--- diy-part1.sh
+++ diy-part1.sh
@@ -67,7 +67,6 @@ sed -i 's/lan\" \"eth1/lan\" \"eth0/g' target/linux/rockchip/armv8/base-files/et
 sed -i "s/eth1' 'eth0/eth0' 'eth1/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 # Auto Update Adguardhome
-now_ver="$(cat package/lean/adguardhome/Makefile | grep "PKG_VERSION:=" | grep -oE "v[0-9.]+")"
 latest_ver="$(curl -L -k --retry 2 --connect-timeout 20 -o - https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)"
-echo -e "Adguardhome Local version: ${now_ver}., cloud version: ${latest_ver}." 
+echo -e "Adguardhome cloud version: ${latest_ver}." 
 sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=${latest_ver}/g" package/lean/adguardhome/Makefile
